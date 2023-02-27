41a59d58a1d7cf842e110f3506df4c4a1d48b984
mahao <928434248@qq.com>
2023-02-27 11:00:00 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const net = require("net");
 const { merge } = require("webpack-merge");
+// const readline = require("readline");
 const baseConfig = require("./build/webpack.base.js");
 const prodConfig = require("./build/webpack.prod.js");
 const devConfig = require("./build/webpack.dev.js");
 
-const isDev = process.env.NODE_ENV === "development"; // 是否是开发模式
+const isDev = process.env.NODE_ENV === "development";
 let PORT = 3000;
+// let portIsUsed = false;
 function checkPort(port) {
   PORT = port;
   const server = net.createServer().listen(port);
@@ -16,11 +18,33 @@ function checkPort(port) {
   });
   server.on("error", err => {
     if (err.code === "EADDRINUSE") {
+      // portIsUsed = true;
       checkPort(port + 1);
     }
   });
 }
 checkPort(PORT);
+
+// function askUser(resolve, portConfig) {
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+//   rl.question(
+//     " Something is already running on port 3000. Would you like to run the app on another port instead? (yes/no) ",
+//     answer => {
+//       if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
+//         return resolve(merge(baseConfig, isDev ? devConfig : prodConfig, portConfig));
+//       }
+//       if (answer.toLowerCase() === "no" || answer.toLowerCase() === "n") {
+//         return process.exit();
+//       }
+//       console.log("Invalid answer. Please enter yes or no!");
+//       rl.close();
+//       return process.exit();
+//     },
+//   );
+// }
 
 module.exports = () => {
   const portConfig = {};
@@ -31,7 +55,7 @@ module.exports = () => {
           port: PORT,
         };
       }
-      resolve(merge(baseConfig, isDev ? devConfig : prodConfig, portConfig));
+      return resolve(merge(baseConfig, isDev ? devConfig : prodConfig, portConfig));
     }, 100);
   });
 };
